35c75f7f21882b9d0a01253116baf2e51fb41177
sajid199 <sajideee2015@gmail.com>
2025-06-09 19:11:16 +0530

    pushed

---
+++ script.js
@@ -0,0 +1,5 @@
+// Placeholder for any future JS functionality
+document.querySelector("form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Search functionality not implemented.");
+});
